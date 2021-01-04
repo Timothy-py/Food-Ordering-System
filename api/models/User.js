ad008320cb950f9bfd590ffe079cf9efc2c04be4
Timothy-py <adeyeyetimothy33@gmail.com>
2021-01-04 23:28:01 +0100

    created functions for encrypting and validating user passwords

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,7 @@
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -37,6 +38,41 @@ module.exports = {
       via: 'owner'
     }
   },
+
+
+  // Function for Encrypting user password
+  encryptPassword(values, next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            sails.log.error(err);
+            return next();
+        }
+
+        bcrypt.hash(values.password, salt, (err, hash) => {
+            if (err) {
+                sails.log.error(err);
+                return next();
+            }
+            values.encryptedPassword = hash; // Here is our encrypted password
+            return next();
+        });
+    });
+},
+
+// Function that compares user typed password and the one encrypted in the db
+comparePassword(password, encryptedPassword) {
+
+    return new Promise(function(resolve, reject) {
+        bcrypt.compare(password, encryptedPassword, (err, match) => {
+            if (err) {
+                sails.log.error(err);
+                return reject("Something went wrong!");
+            }
+            if (match) return resolve();
+            else return reject("Mismatch passwords");
+        });
+    });
+}
 
 };
 
